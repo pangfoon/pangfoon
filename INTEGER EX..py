243580f9c2489f441d430939a13641a8043cdf9f
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-07-05 16:04:55 +0700

    Add files via upload

---
+++ INTEGER EX..py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print(18/2)
+print(18-2)
+print(18+2)
+print(18*2)
+
+
+# In[6]:
+
+
+print(type(18/2))
+print(type(18-2))
+print(type(18+2))
+print(type(18*2))
+
+
+# In[ ]:
+
+
+
+
