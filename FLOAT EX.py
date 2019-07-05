3591dd25044ca02ca9177affabb18b974e875ca0
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-07-05 16:06:37 +0700

    Add files via upload

---
+++ FLOAT EX.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(type(3.14))
+
+
+# In[2]:
+
+
+print(10+3.14)
+
+
+# In[3]:
+
+
+print(10+3.0)
+
+
+# In[4]:
+
+
+print(10+0.3)
+print(15*1.0)
+print(15.1*2)
+
+
+# In[ ]:
+
+
+
+
