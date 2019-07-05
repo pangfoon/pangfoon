076fc54fd2911ae0edb77a197db7c80194d3898a
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-07-05 16:07:21 +0700

    Add files via upload

---
+++ STRING EX.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("HELLO")
+print(type("HELLO"))
+
+
+# In[2]:
+
+
+print("HELLO"+"WORLD")
+
+
+# In[4]:
+
+
+print("HELLO"-"WORLD")
+
+
+# In[5]:
+
+
+print("HELLO"*5)
+
+
+# In[6]:
+
+
+print("HELLO"/5)
+
+
+# In[ ]:
+
+
+
+
