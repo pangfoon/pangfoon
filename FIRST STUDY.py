0dcc37a285847d5344ea718ced85dd8298516bd9
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-07-05 14:06:40 +0700

    Add files via upload
    FIRST PROGRAM

---
+++ FIRST STUDY.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("-----------------------------")
+print("PANG SHOP")
+print("-----------------------------")
+print("1. milk              x 1   15 THB   = 15 THB")
+print("2. sandwich       x2    10 THB   = 20 THB")
+print("3. icecream       x3    10 THB    = 30 THB")
+print("-----------------------------")
+print("Total                                      = 65 THB")
+
+
+
+# In[3]:
+
+
+print(10)
+
+
+# In[4]:
+
+
+print(type(10))
+
+
+# In[5]:
+
+
+print(type("HELLO"))
+
+
+# In[6]:
+
+
+print(type(True))
+
+
+# In[7]:
+
+
+print(type(10.3))
+
+
+# In[8]:
+
+
+print(type(10+3.5))
+
+
+# In[9]:
+
+
+print((10+3.5))
+
+
+# In[ ]:
+
+
+
+
