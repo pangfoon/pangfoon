818955ccd7f2096e0cc36bb4c9005d8fa7d0b7f4
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-07-05 16:07:42 +0700

    Add files via upload

---
+++ INCLUDE INFORMATION.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("------------- PANGFOON SHOP---------------")
+print("---------------------------------------------------")
+print("MILK              x1      12 THB   =  12 THB")
+print("ICECREAM      x1      20 THB   =  20 THB")
+print("CANDY           x10     5 THB    =  50 THB")
+print("---------------------------------------------------")
+print(" TOTAL                                 =   82 THB")
+print("---------------- THANK YOU -----------------")
+
+
+# In[12]:
+
+
+print("------------- PANGFOON SHOP---------------")
+print("---------------------------------------------------")
+print("MILK" , 1 , 12 ,     "THB",     (1*12)  ,"THB")
+print("ICECREAM" , 1 , 20 ,      "THB ",      (1*20) , "THB")
+print("CANDY" , 10 , 5 ,      "THB",     (10*5) ,"THB")
+print("---------------------------------------------------")
+print(" TOTAL" , (12+20+50)  ,    "THB")
+print("---------------- THANK YOU -----------------")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
