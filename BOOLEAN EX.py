cbd503a74c52c7c397685a2a02d72b13fe3e0ac7
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-07-05 16:07:06 +0700

    Add files via upload

---
+++ BOOLEAN EX.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(True)
+print(False)
+
+
+# In[4]:
+
+
+print(type(True))
+print(type(False))
+
+
+# In[5]:
+
+
+print(False and True)
+
+
+# In[6]:
+
+
+print(False or True)
+
+
+# In[ ]:
+
+
+
+
