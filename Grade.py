4ddd716bd478b947f7407a70f5184b041be75e5a
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-08-30 14:09:25 +0700

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#โปรแกรมการตัดเกรด
+SCORE=float(input("STUDENT SCORE:"))
+if 80<=SCORE<=100:
+    print("A")
+elif 75<=SCORE<=80:
+    print("B+")
+elif 70<=SCORE<=75:
+    print("B")
+elif 65<=SCORE<=70:
+    print("C+")
+elif 60<=SCORE<=65:
+    print("C")
+elif 55<=SCORE<=60:
+    print("D+")
+elif 50<=SCORE<=55:
+    print("D")
+elif 0<=SCORE<=50:
+    print("F")
+#ถ้าใส่คะแนนเกินโปรแกรมจะทำงานส่วนของ else 
+else:
+    print("ERROR")
+    
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+69.95
+
