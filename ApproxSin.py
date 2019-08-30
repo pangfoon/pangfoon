ee2ea03c9215e8ee13711166a5b309b5f7b5996b
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-08-30 15:49:49 +0700

    Add files via upload

---
+++ ApproxSin.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#โปรแกรมคำนวณด้วยอนุกรมเทย์เลอร์
+import math
+
+x = float(input())
+x = math.radians(x%360)
+s = f = x 
+k = 1
+while f != 0:
+    f *= (-1)*x**2/((2*k)*(2*k+1))
+    s += f
+    k += 1
+print(s)
+
+
+# In[ ]:
+
+
+
+
