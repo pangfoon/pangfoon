8622f09a993602d603db3ff9e14ad32b551047a6
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-09-06 22:08:42 +0700

    Add files via upload

---
+++ AVG.py
@@ -0,0 +1,16 @@
+sum = 0
+count = 0
+n = float(input("enter number"))
+while n != -1:
+    sum += n
+    count = count + 1
+    n = float(input("enter number"))
+avg = sum / count
+print(avg)
+
+
+# In[ ]:
+
+
+
+
