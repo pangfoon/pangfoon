cc33abcb0225c977a45679b2b0e98f1320250f34
pangfoon <52314681+pangfoon@users.noreply.github.com>
2019-09-06 22:36:28 +0700

    Add files via upload

---
+++ Triang3.py
@@ -0,0 +1,18 @@
+In[25]:
+
+
+# à¸™à¸²à¸¢ à¸§à¸µà¸£à¸°à¸¨à¸±à¸à¸”à¸´à¹Œ à¸§à¸‡à¸©à¹Œà¸ à¸¹à¹ˆà¸‡à¸²à¸¡ 362515241014 EE36241N
+max = 0
+i = 3
+sum = 0
+for i in range(i):
+    if angle > max:
+        max = angle
+    angle = int(input("enter angle :"))
+    sum = sum + angle
+print("sum :", sum)
+print("hypotenuse", max)
+        
+    
+    
+    
